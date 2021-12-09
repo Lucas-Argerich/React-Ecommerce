15b240de0859d05c477af97281bbb31f359eebd6
Lucas Argerich <lucasargerichcoder@gmail.com>
2021-12-09 14:07:10 -0300

    Empty cart sign

--- src/components/cartList/index.jsx
+++ src/components/cartList/index.jsx
@@ -6,11 +6,19 @@ import './index.scss'
 export default function CartList() {
     const { cart } = useCart()
     return (
+
         <div className="cartList">
             <h2>Cart</h2>
-            {cart.addedItems.map(i =>
-                <CartListItem key={i.item.id} id={i.item.id} title={i.item.title} price={i.item.price} image={i.item.image} quantity={i.quantity}></CartListItem>
-            )}
+            {
+                cart.addedItems.length > 0 ?
+
+                    cart.addedItems.map(i =>
+                        <CartListItem key={i.item.id} id={i.item.id} title={i.item.title} price={i.item.price} image={i.item.image} quantity={i.quantity}></CartListItem>
+                    )
+                    :
+                    <h3>Feel free to add items to your cart!</h3>
+            }
         </div>
+
     )
 }
