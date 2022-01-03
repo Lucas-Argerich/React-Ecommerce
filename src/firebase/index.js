beb0f794327a05d7ecb24fcd4c5b69408f805cab
Lucas-Argerich <88691650+Lucas-Argerich@users.noreply.github.com>
2022-01-03 13:38:29 -0300

    Firebase Config Fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
-import keys from './keys.js'
 
-const firebaseApp = initializeApp(keys)
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCnIJJI3Uyv2U_1Z60wKWIAlT66lUDGQJc",
+  authDomain: "react-ecommerce-coderhou-17779.firebaseapp.com",
+  projectId: "react-ecommerce-coderhou-17779",
+  storageBucket: "react-ecommerce-coderhou-17779.appspot.com",
+  messagingSenderId: "1007285532751",
+  appId: "1:1007285532751:web:b5fc5e09f83772ee36923a",
+  measurementId: "G-XWD85W6KJH"
+})
 
 export const db = getFirestore()
